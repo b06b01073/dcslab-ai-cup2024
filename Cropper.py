46038ca445669d48305dc18b7b44adffb9460971
jonathan0302 <jonathan0302.cs12@nycu.edu.tw>
2024-05-21 00:16:26 +0800

    Add conditions to check bounding boxes zone and size
    Add inZone() to check if a bounding box is in locations where cars shouldn't be.
    Add sizeCheck() to check if a bounding box is bigger than a certain threshold that can be passed in as a parameter, with a default value of 0.

--- Cropper.py
+++ Cropper.py
@@ -2,11 +2,14 @@ import torch
 import torchvision.transforms.functional as F
 from torchvision import transforms
 from torchvision.io import read_image
+from shapely.geometry import Polygon
 
 class Cropper():
 
-    def __init__(self, img_width):
+    def __init__(self, img_width, cam, min_size):
         self.img_width = img_width
+        self.cam = cam
+        self.min_size = min_size
 
     def convert(self, W, H, x_center_norm, y_center_norm, w_norm, h_norm):
 
@@ -58,12 +61,40 @@ class Cropper():
         h_norm = float(info[4])
         
         return x_center_norm, y_center_norm, w_norm, h_norm
+    
+    def inZone(self,left,top,w,h):
+        cam0 = [[(0,0), (0,128), (98,186), (348,118), (430,115),(668,0)], [(747,0), (690,145), (730,148), (760,0)], [(816,0), (1083,195), (1280,234),(1280,0)]]
+        cam1 = [[(0,0), (0,137), (1024,276), (1280,248)], [(0,172), (0,219), (719,396), (824,358)]]
+        cam2 = [[(0,0), (0,322), (81,298), (422,98), (422,0)], [(476,0), (484,308), (611,295), (543,163), (543,0)], [(562,0), (562,99), (1077,299), (1280,325), (1280,0)]]
+        cam3 = [[(0,0), (0,345), (260,267), (260,0)], [(5,720), (340,272), (439.720)], [(366,0), (366,272), (1280,476), (1280,0)]]
+        cam4 = [[(0,0), (0,356), (728,0)], [(783,0), (620,363), (734,366)], [(857,0), (1280,356), (1280,0)]]
+        cam5 = [[(0,0), (0,283), (74,265), (58,0)], [(0,330), (0,642), (209,590), (109,324)], [(58,0), (68,183), (666,276), (1280,237), (1280,0)], [(731,308), (1280,428), (1280,278)]]
+        cam6 = [[(0,0), (0,170), (1280,163), (1280,0)], [(0,200), (0,278), (164,195)], [(712,183), (1058,326), (1280,337), (1280,190)]]
+        cam7 = [[(0,97), (0,327), (260,175)], [(310,190), (312,720), (452,720)], [(0,0), (1280,478), (0,97), (1280,478), (1280,0)]]
+
+        zones = [cam0, cam1, cam2, cam3, cam4, cam5, cam6, cam7]
+
+        bbox = Polygon([(left, top), (left+w, top), (left+w, top+h), (left, top+h)])
+        for polygon in zones[self.cam]:
+            poly = Polygon(polygon)
+            if poly.contains(bbox):
+                return True
+            else:
+                return False
+    
+    def sizeCheck(self, w, h):
+        if w * h >= self.min_size:
+            return True
+        else:
+            return False
 
 
     def crop_frame(self, image_path, label_path):
 
         """
         Crop regions of interest from the image based on bounding box information.
+        Will use the coordinates of boudning boxes to determine if they're in the zones not interested in and if they're big enough to be considered. 
+        This function will use the shapely library, might need to install it.
 
         Args:
         - image_path (str): Path to the image file.
@@ -91,15 +122,19 @@ class Cropper():
             info = info.split(' ')
             x_center_norm, y_center_norm, w_norm, h_norm = self.get_image_info(info)
             left, top, w, h = self.convert(W, H, x_center_norm, y_center_norm, w_norm, h_norm)
-            info_list.append([left, top, left+w, top+h])
-            info_list_norm.append([x_center_norm, y_center_norm, w_norm, h_norm])
-            # Crop the image region based on the bounding box coordinates
-            croped_img = (F.crop(image, top, left, h, w))/255
-            transform = transforms.Compose([
-                transforms.Resize((self.img_width, self.img_width))
-            ])
-            croped_img = transform(croped_img)
-            cropped_regions = torch.cat((cropped_regions, torch.unsqueeze(croped_img, 0)))
+            
+            if not self.inZone(left, top, w, h) and self.sizeCheck(w, h):
+                info_list.append([left, top, left+w, top+h])
+                info_list_norm.append([x_center_norm, y_center_norm, w_norm, h_norm])
+                # Crop the image region based on the bounding box coordinates
+                croped_img = (F.crop(image, top, left, h, w))/255
+                transform = transforms.Compose([
+                    transforms.Resize((self.img_width, self.img_width))
+                ])
+                croped_img = transform(croped_img)
+                cropped_regions = torch.cat((cropped_regions, torch.unsqueeze(croped_img, 0)))
             info = label.readline()
         
         return cropped_regions, info_list, info_list_norm
+    
+    
