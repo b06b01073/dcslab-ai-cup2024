af0e78524c988facd079cd10175c20384a67bab5
Jonas0126 <jonaslaisad@gmail.com>
2024-06-08 14:42:52 +0800

    Modify some files to make them executable in Colab.

--- multi_track.sh
+++ multi_track.sh
@@ -6,11 +6,11 @@ date_list=(0902_130006_131041 0902_180000_181551 0903_125957_131610 0903_175958_
 for date in "${date_list[@]}";
     do
         echo python multi_match.py --date $date --model swin_reid --mode min --finetune True -t 40
-        python multi_match.py --date $date --model swin_reid --mode min --finetune True -t 40
+        python multi_match.py --date $date --model swin_reid --mode min --finetune True -t 40 -f ../32_33_AI_CUP_testdataset/AI_CUP_testdata/images/$date -l RE_RESULT/labels/$date --out MULTI_MATCH_RESULT
 
 done
 
 for date in "${date_list[@]}";
 do
-    python tools/datasets/AICUP_to_MOT15.py --AICUP_dir RE_FINAL_result_v3/labels/swin_reid_min_40/"$date" --MOT15_dir MOT15/RE_firstSub_v3/
+    python tools/datasets/AICUP_to_MOT15.py --AICUP_dir MULTI_MATCH_RESULT/labels/swin_reid_min_40/"$date" --MOT15_dir MOT15/MULTI_MATCH_RESULT/
 done
